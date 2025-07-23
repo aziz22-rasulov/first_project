c32a8c4e9e1581b264788543e0b095bbd7e7005c
Aziz Rasulov <147405914+aziz22-rasulov@users.noreply.github.com>
2025-07-23 19:33:35 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ with col2:
 
 
 x = df.drop(['species'], axis=1)
-y = df['species]
+y = df['species']
 X_train, X_test, y_train, y_test = train_test_split (X, y, test_size=0.3, random_state=42)
 
 
