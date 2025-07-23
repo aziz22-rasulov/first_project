b4a670b2c19d5f9ea4eda80164a341cd0f79c1d6
Aziz Rasulov <147405914+aziz22-rasulov@users.noreply.github.com>
2025-07-23 18:48:26 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,15 @@
 import streamlit as st
+import pandas as pd
+import sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborClassifier
+import category_encoders as ce
+import plotly.express as px
 
+st.set_page_config(page_title='ff', layout='wide')
 st.title('🎈 App Name')
 
-st.write('Привет Мир!')
+st.write('Работа с датасетом Пингвинов')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
